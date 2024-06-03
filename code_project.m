c3ceb824591f6abbf30de891edf691dc91424af7
Dabne Barrera <dibarrera@uc.cl>
2024-06-03 16:52:38 -0400

    nuevos plots

--- code_project.m
+++ code_project.m
@@ -3,18 +3,34 @@ clc
 clear
 close all
 %% read and view data
-load("project_data.mat")
+load("project_data_v2.mat")
 figure;
+sgtitle('bright blood')
 subplot(2,2,1)
-imshow(patient1_sys(:,:,30),[])
+imshow(patient1_sys_brightblood(:,:,30),[])
 title('patient 1 systole')
 subplot(2,2,2)
-imshow(patient2_sys(:,:,30),[])
+imshow(patient2_sys_brightblood(:,:,30),[])
 title('patient 2 systole')
 subplot(2,2,3)
-imshow(patient1_dia(:,:,30),[])
+imshow(patient1_dia_brightblood(:,:,30),[])
 title('patient 1 diastole')
 subplot(2,2,4)
-imshow(patient2_dia(:,:,30),[])
+imshow(patient2_dia_brightblood(:,:,30),[])
+title('patient 2 diastole')
+
+figure;
+sgtitle('black blood')
+subplot(2,2,1)
+imshow(patient1_sys_blackblood(:,:,30),[])
+title('patient 1 systole')
+subplot(2,2,2)
+imshow(patient2_sys_blackblood(:,:,30),[])
+title('patient 2 systole')
+subplot(2,2,3)
+imshow(patient1_dia_blackblood(:,:,30),[])
+title('patient 1 diastole')
+subplot(2,2,4)
+imshow(patient2_dia_blackblood(:,:,30),[])
 title('patient 2 diastole')
 %%
